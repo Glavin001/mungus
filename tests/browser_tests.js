f2bd1f895b6f84d1c11ffe1fb3583b22f88abf02
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-04-14 02:36:02 -0300

    Added support for  in Find.

--- tests/browser_tests.js
+++ tests/browser_tests.js
@@ -13,7 +13,7 @@ test("basic query", function() {
 
   var results = arr.find({a:1});
   var r = results.length === 2;
-  console.log(results, results.length, r);
+  //console.log(results, results.length, r);
   ok( r, "Passed! Returned only 2 results.");
 });
 
@@ -29,7 +29,7 @@ test("compound query", function() {
     b: 2
   });
   var r = results.length === 1;
-  console.log(results, results.length, r);
+  //console.log(results, results.length, r);
   ok( r, "Passed! Returned only 1 results.");
 });
 
@@ -45,8 +45,44 @@ test("nested query", function() {
     "a.b": 2
   });
   var r = results.length === 1;
-  console.log(results, results.length, r);
+  //console.log(results, results.length, r);
   ok( r, "Passed! Returned only 1 results.");
+});
+
+module("projection");
+
+test("nested query with projection", function() {
+  var arr = [
+    {a: { b:2 }, c: 3 },
+    {a:1, c:3},
+    {a:2, b:2}
+    ];
+
+  var results = arr.find({
+    "a.b": 2
+  }, {
+    a: 0,
+    c: 1
+  });
+  var r = results.length === 1 && results[0].c === 3;
+  //console.log(results, r);
+  ok( r, "Passed!");
+});
+
+test("renaming projection", function() {
+  var arr = [
+    {a: { b: 1 }, c: 3 },
+    {a: { b: 2 }, c:3},
+    {a: 3, b:2}
+    ];
+
+  var results = arr.find({"a.b":2}, {
+    d: "$a.b",
+    c: 1
+  });
+  var r = results.length === 1 && results[0].d === 2;
+  //console.log(results, results.length, r);
+  ok( r, "Passed!");
 });
 
 
